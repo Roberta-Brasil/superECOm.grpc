c22e742d5af79df7a6f34f3b61e3369fdeaebefe
Roberta-Brasil <robertabrasil.dev@gmail.com>
2022-04-21 08:40:57 -0300

    grpc importing added.

--- src/main/java/ds/SearchingByLocation/searchingByLocationGrpc.java
+++ src/main/java/ds/SearchingByLocation/searchingByLocationGrpc.java
@@ -198,8 +198,7 @@ public final class searchingByLocationGrpc {
     }
   }
 
-  /**
-   */
+  /*import from server class  */
   public static final class searchingByLocationStub extends io.grpc.stub.AbstractStub<searchingByLocationStub> {
     private searchingByLocationStub(io.grpc.Channel channel) {
       super(channel);
